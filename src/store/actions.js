d63dde77c3165dc7ac45122d4fffca6602dd2981
fredsalas87 <fredsalas87@gmail.com>
2023-03-23 12:06:46 -0300

    Melhorando arquivo do Vuex

---
+++ src/store/actions.js
@@ -0,0 +1,36 @@
+import db from '../services/localbase.js'
+
+export default {
+  async adicionarTarefa({ commit }, titulo) {
+    await commit("adicionarTarefa", titulo);
+    await commit("buscaTarefas");
+  },
+  editaTarefa({ commit }, novaTarefa) {
+    db.collection("tarefas")
+      .doc({ id: novaTarefa.id })
+      .update({
+        titulo: novaTarefa.titulo,
+      })
+      .then(() => {
+        commit("buscaTarefas");
+      });
+  },
+  concluiTarefa({ commit }, novaTarefa) {
+    db.collection("tarefas")
+      .doc({ id: novaTarefa.id })
+      .update({
+        concluido: !novaTarefa.concluido,
+      })
+      .then(() => {
+        commit("buscaTarefas");
+      });
+  },
+  removeTarefa({ commit }, id) {
+    db.collection("tarefas")
+      .doc({ id })
+      .delete()
+      .then(() => {
+        commit("buscaTarefas");
+      });
+  },
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,57 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import Localbase from "localbase";
-
-let db = new Localbase("db");
+import state from './state.js'
+import mutations from './mutations.js'
+import actions from './actions.js'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    tarefas: [
-      // {id:1, titulo: 'Ir ao mercado', concluido: false},
-      // {id:2, titulo: 'Ir à padaria', concluido: false},
-    ],
-  },
-  mutations: {
-    buscaTarefas(state) {
-      db.collection("tarefas")
-        .get()
-        .then((tarefasDB) => {
-          state.tarefas = tarefasDB;
-        });
-    },
-    adicionarTarefa(state, titulo) {
-      db.collection("tarefas").add({
-        id: new Date().getTime(),
-        titulo,
-        concluido: false,
-      });
-    },
-  },
-  actions: {
-    async adicionarTarefa({ commit }, titulo) {
-      await commit("adicionarTarefa", titulo);
-      await commit("buscaTarefas");
-    },
-    editaTarefa({ commit }, novaTarefa) {
-      db.collection("tarefas")
-        .doc({ id: novaTarefa.id })
-        .update({
-          titulo: novaTarefa.titulo,
-        })
-        .then(() => {
-          commit("buscaTarefas");
-        });
-    },
-    removeTarefa({ commit }, id) {
-      db.collection("tarefas")
-        .doc({ id })
-        .delete()
-        .then(() => {
-          commit("buscaTarefas");
-        });
-    },
-  },
+  state,
+  mutations,
+  actions,
   modules: {},
 });
